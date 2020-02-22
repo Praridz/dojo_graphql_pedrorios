e9717e9bdf8508b439fb77e9a99973aef4dd5d6d
Praridz <rios.371@hotmail.com>
2020-02-22 06:34:44 -0500

    first commit dojo graphql pedro rios

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require ("apollo-server");
+const PostApi = require("./DataSource/PostAPI")
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+
+const server = new ApolloServer({
+    typeDefs,resolvers, dataSources: () => new PostApi()
+});
+
+server.listen().then(({ url}) => {
+    console.log(`Server ready at ${url}`)
+})
